986a9994469872f497383b3265b81d67bbbdf28a
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-12 01:35:30 +0700

    feat: finished exercise 1.2

---
+++ src/Exercise_1_2/App/CircleApp.java
@@ -0,0 +1,21 @@
+package Exercise_1_2.App;
+
+import Exercise_1_2.Data.Circle;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(1.1);
+        System.out.println(c1);   // toString()
+        Circle c2 = new Circle(); // default constructor
+        System.out.println(c2);
+
+        // Test setter and getter
+        c1.setRadius(2.2);
+        System.out.println(c1);      // toString()
+        System.out.println("radius is: " + c1.getRadius());
+
+        // Test getArea() and getCircumference()
+        System.out.printf("area is: %.2f%n", c1.getArea());
+        System.out.printf("circumference is: %.2f%n", c1.getCircumference());
+    }
+}
