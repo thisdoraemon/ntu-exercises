1255eb30b177c2f0039cb2da4e480942e892adcd
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-13 06:48:42 +0700

    feat: added Exercise 1.3

---
+++ src/Exercise_1_3/Data/Rectangle.java
@@ -0,0 +1,44 @@
+package Exercise_1_3.Data;
+
+public class Rectangle {
+    private float length = 1.0f;
+    private float width = 1.0f;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle[length=" + length + ", width=" + width + "]";
+    }
+}
