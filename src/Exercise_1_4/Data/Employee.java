8e18cd3095e109be50132365359965e0345d5cb4
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-13 23:40:31 +0700

    feat: added exercise 1.4

---
+++ src/Exercise_1_4/Data/Employee.java
@@ -0,0 +1,53 @@
+package Exercise_1_4.Data;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAnnualSalary() {
+        return salary * 12;
+    }
+
+    public int raiseSalary(int percent) {
+        double increaseAmount = (salary * percent) / 100.0;
+        return (int) (salary + increaseAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[id=" + id + ", name=" + firstName + " lastname=" + lastName + ", salary=" + salary + "]";
+    }
+}
