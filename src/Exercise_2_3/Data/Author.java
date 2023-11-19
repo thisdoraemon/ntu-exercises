d4ab6d52a69bc279daf67dd3cc78557e5f0f5999
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-19 14:52:16 +0700

    feat: added Exercise 2.3

---
+++ src/Exercise_2_3/Data/Author.java
@@ -0,0 +1,28 @@
+package Exercise_2_3.Data;
+
+public class Author {
+    private String name;
+    private String email;
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Author[name=" + name + ",email=" + email + "]";
+    }
+}
