4f179c57e5de8475a30eb1494d7b39a8596b48f8
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-24 16:54:37 +0700

    feat: added Exercise 3.3

---
+++ src/Exercise_3_3/App/TestBigInteger.java
@@ -0,0 +1,16 @@
+package Exercise_3_3.App;
+
+import java.math.BigInteger;
+
+public class TestBigInteger {
+    public static void main(String[] args) {
+        BigInteger num1 = new BigInteger("11111111111111111111111111111111111111111111111111111111111111");
+        BigInteger num2 = new BigInteger("22222222222222222222222222222222222222222222222222");
+
+        BigInteger sum = num1.add(num2);
+        System.out.println("Sum: " +  sum);
+
+        BigInteger product = num1.multiply(num2);
+        System.out.println("Product: " + product);
+    }
+}
